f7ecc903deb7173b52b81630e8609bda24a8caa5
rfmineguy <rfmineguy@gmail.com>
2023-08-01 19:10:48 -0700

    deleted build

--- build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/rileyfischer/Documents/dev/games/mirrors/build/_deps/raylib-src/src
-# Build directory: /Users/rileyfischer/Documents/dev/games/mirrors/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
